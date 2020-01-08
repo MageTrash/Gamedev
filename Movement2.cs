3f528b11ee986c8d8f7168f46c7b96690ac2a41c
Mage <59563439+MageTrash@users.noreply.github.com>
2020-01-09 02:00:53 +1300

    Update Movement2.cs
    mostly upgraded the movement possibilities, still a little jittery

--- Movement2.cs
+++ Movement2.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +7,27 @@ public class Movement2 : MonoBehaviour
 
     public float moveSpeed = 5f;
     public float jumpStrength = 2f;
+    public float swimSpeed = 10f;
+    public float swimDrag = 5f;
     public float gravitonus = 0.5f;
+    public float swimitonus = 0.1f;
     public Rigidbody2D rb;
-    private bool isGrounded = true;
     private bool jump = false;
+    private bool isGrounded = true;
+    private bool isSwimming = false;
 
-    //Ground check via box collider
+    //Ground check and Swimming check
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+        }
+
+        if (other.gameObject.CompareTag("Liquid"))
+        {
+            isSwimming = true;
+            rb.velocity = new Vector2(0f, 0f);
         }
     }
 
@@ -27,15 +37,15 @@ public class Movement2 : MonoBehaviour
         {
             isGrounded = false;
         }
+
+        //Swimming
+        if (other.gameObject.CompareTag("Liquid"))
+        {
+            isSwimming = false;
+        }
     }
 
     private void Update()
-    { 
-        Debug.Log("Grounded = " + isGrounded);
-        Debug.Log("Jump = " + jump); //Minor checks
-    }
-    //Physics involved motion
-    private void FixedUpdate()
     {
         //Button assignment
         if (Input.GetButton("Vertical") || Input.GetButton("Jump"))
@@ -45,13 +55,22 @@ public class Movement2 : MonoBehaviour
         else
         {
             jump = false;
-
         }
+        if (isSwimming == false)
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            //transform.position += movement * Time.deltaTime * moveSpeed;
+            transform.Translate(movement * Time.deltaTime * moveSpeed);
+        }
+        //Debug checks
+        Debug.Log("Swimming = " + isSwimming);
+        Debug.Log("Grounded = " + isGrounded);
+        Debug.Log("Jump = " + jump);
+    }
 
-        //Horizontal motion
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
-
+    //Physics involved motion
+    private void FixedUpdate()
+    {
         //Jump motion
         if ((jump == true) && (isGrounded == true))
         {
@@ -63,26 +82,30 @@ public class Movement2 : MonoBehaviour
                 ///     isGrounded = false
             }
         }
-
-        //Gliding
-        if (Input.GetKey(KeyCode.LeftShift))
+        //Gliding 
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && (rb.velocity.y < -2f) && (isSwimming == false))
         {
             rb.gravityScale = gravitonus;
+
+
+        }
+        //Swimming
+        else if (isSwimming == true)
+        {
+            rb.velocity = new Vector2(0f, -swimitonus);
+            float moHorz = Input.GetAxis("Horizontal");
+            float moVert = Input.GetAxis("Vertical");
+            Vector2 swimMovement = new Vector2(moHorz, moVert);
+            //rb.AddForce(swimMovement * swimSpeed);
+
+            //transform.position += movement * Time.deltaTime * swimSpeed;
+            transform.Translate(swimMovement * swimSpeed / swimDrag);
+
+            isGrounded = false;
         }
         else
         {
             rb.gravityScale = 1.5f;
         }
-
-
     }
-
-
-
-
-
-
-
-
-
 }
